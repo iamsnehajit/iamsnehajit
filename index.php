3d7db7a101b54e13053a337448afa290c234953c
iamsnehajit <71019683+iamsnehajit@users.noreply.github.com>
2021-04-18 23:12:51 +0530

    new commit

---
+++ index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jagadishpur Co-operative Credit Society Limited</title>
+	<?php include ('header-include.php'); ?>
+</head>
+<body>
+	<div class="container-fluid p-0">
+		<div class="banner-head">
+      			<h1 class="large-txt uppercase white Playfair">Kona Chamrail Jagadishpur Co-operative Credit Society Limited</h1>
+			<div class="office-address">
+				<p class="white">Registered Office: Vill. Khalia, P.O. Chamrail, Dist. Howrah, Pin- 711114, West Bengal</p>
+				<p class="white">Reg No - 35 dated -19<sup>th</sup> June, 1989</p>
+			</div>
+      		</div>
+	</div>
+	<?php include ('header.php'); ?>
+	<!------==============Banner===========------------>
+	<div class="container-fluid p-0">
+		<div class="index-banner">
+			<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  			<ol class="carousel-indicators">
+    			<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    			<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    			<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  			</ol>
+  			<div class="carousel-inner">
+    			<div class="carousel-item active">
+      			<img class="d-block w-100" src="images/naf.jpg" alt="First slide">
+    			</div>
+    			<div class="carousel-item">
+      			<img class="d-block w-100" src="images/2.jpg" alt="Second slide">
+      			
+    			</div>
+			<div class="carousel-item">
+      			<img class="d-block w-100" src="images/1.jpg" alt="Second slide">
+      			
+    			</div>
+    			<div class="carousel-item">
+      			<img class="d-block w-100" src="images/8.jpg" alt="Third slide">
+      			
+    			</div>
+  			</div>
+			</div>
+		</div>
+	</div>
+
+	<!--------======Body top========-------->
+	<div class="container p-0">
+		<div class="index-about">
+			<div class="row">
+				<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 p-0">
+					<div class="index-banner-left">
+						<h2 class="large-txt uppercase">Welcome to KCJCCSLTD</h2>
+						<p class="normal-txt">Kona Chamrail Jagadishpur Co-operative Credit Society Limited (KCJCCSLTD) is situated at village Khalia under PS of Liluah. The post office is Chamrail and the nearest railway station, Kona is about 1.5 km away from the area.We have three branch offices at Kona, Chamrail and Jagadishpur. We earnestly believed the principle of the Cooperative Society "Live and Let Live" and rigorously persued several objectives namely:</p>
+						<ul>
+							<li>to identify the need of the villagers and try to solve them;</li>
+							<li>to depict the duties of the members with sincerity, honesty and self reliability;</li>
+							<li>to plan properly and execute with the support of the people and</li>
+							<li>to evaluate the performance on regular basis.INFRASRUCTURE AND PROGRESS</li>
+							<li>to incresing social security through life insurence.</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 p-0">
+					<div class="index-banner-right">
+						<img src="images/mi.jpg" alt="Image"/>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!---------=========Slider=======---------->
+	<div class="container p-0">
+		<div class="owl-slider-index">
+			<div class="slider-banner text-center">
+				<h3 class="uppercase large-txt">our achievements</h3>
+			</div>
+			<div class="owl-carousel owl-theme">
+			    <div class="item">
+			    	<img src="images/aj.jpg" alt="image"/>
+			    </div>
+			    <div class="item">
+			    	<img src="images/aj2.jpg" alt="image"/>
+			    </div>
+			    <div class="item">
+			    	<img src="images/aj3.jpg" alt="image"/>
+			    </div>
+			    <div class="item">
+			    	<img src="images/ajjkal.jpg" alt="image"/>
+			    </div>
+			    <div class="item">
+			    	<img src="images/ajj-kal-three.jpg" alt="image"/>
+			    </div>
+			    <div class="item">
+			    	<img src="images/ajj-kal-two.jpg" alt="image"/>
+			    </div>
+			</div>
+		</div>
+	</div>
+
+	<!---=======Gallery========----------->
+	<div class="container p-0">
+		<div class="gallery-index">
+			<div class="slider-banner text-center">
+				<h4 class="uppercase large-txt">our gallery</h4>
+				<div class="row">
+					<div class="col-lx-3 col-lg-3 col-md-3 col-sm-3 p-0">
+						<div class="gal-index-img">
+							<img src="images/arup-ray.jpg" alt="images"/>
+						</div>
+					</div>
+					<div class="col-lx-3 col-lg-3 col-md-3 col-sm-3 p-0">
+						<div class="gal-index-img">
+							<img src="images/arup-ray.jpg" alt="images"/>
+						</div>
+					</div>
+					<div class="col-lx-3 col-lg-3 col-md-3 col-sm-3 p-0">
+						<div class="gal-index-img">
+							<img src="images/arup-ray.jpg" alt="images"/>
+						</div>
+					</div>
+					<div class="col-lx-3 col-lg-3 col-md-3 col-sm-3 p-0">
+						<div class="gal-index-img">
+							<img src="images/arup-ray.jpg" alt="images"/>
+						</div>
+					</div>
+					<div class="col-lx-3 col-lg-3 col-md-3 col-sm-3 p-0">
+						<div class="gal-index-img">
+							<img src="images/arup-ray.jpg" alt="images"/>
+						</div>
+					</div>
+					<div class="col-lx-3 col-lg-3 col-md-3 col-sm-3 p-0">
+						<div class="gal-index-img">
+							<img src="images/arup-ray.jpg" alt="images"/>
+						</div>
+					</div>
+					<div class="col-lx-3 col-lg-3 col-md-3 col-sm-3 p-0">
+						<div class="gal-index-img">
+							<img src="images/arup-ray.jpg" alt="images"/>
+						</div>
+					</div>
+					<div class="col-lx-3 col-lg-3 col-md-3 col-sm-3 p-0">
+						<div class="gal-index-img">
+							<img src="images/arup-ray.jpg" alt="images"/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!--=========Acts & Rules==========---------->
+	<div class="container p-0">
+		<div class="act-ruls text-center">
+			<h5 class="uppercase large-txt">Acts & Rules</h5>
+			<div class="row">
+				<div class="col-sm-6 col-md-6 col-lg-6 col-xl-6">
+					<div class="act-rul-each">
+						<a href="images/WBCSRules.pdf" target="_blank">WBCS Rules</a>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-6 col-lg-6 col-xl-6">
+					<div class="act-rul-each">
+						<a href="images/WBSCBAct.pdf" target="_blank">WBSCB Act</a>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-6 col-lg-6 col-xl-6">
+					<div class="act-rul-each">
+						<a href="by-loss.php" target="_blank">Bye Laws</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!---==========Social links========----->
+	<div class="fix-social">
+		<ul class="none-ul">
+			<li><a href="https://www.facebook.com/profile.php?id=100010771368790" target="_blank"><i class="fa fa-facebook-official"></i></a></li>
+			<li><a href="" target="_blank"><i class="fa fa-linkedin-square"></i></a></li>
+			<li><a href="" target="_blank"><i class="fa fa-twitter-square"></i></a></li>
+			<li><a href="https://api.whatsapp.com/send?phone=+91 9836628962&text= Hello" target="_blank"><i class="fa fa-whatsapp"></i></a></li>
+			<li><a href="" target="_blank"><i class="fa fa-instagram"></i></a></li>
+		</ul>
+	</div>
+	<?php include ('footer.php'); ?>
+	<?php include ('footer-include.php'); ?>
+
+	<script>
+		$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay: true,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+	</script>
+</body>
+</html>
